fcddceaabb239247373b20eb431fdbaf6872c61e
durw4rd <misa.fasa@gmail.com>
2016-05-02 11:27:13 +0200

    add prerequisities from W; create gamemodel

---
+++ src/models/GameModel.js
@@ -0,0 +1,20 @@
+import BaseModel from './BaseModel';
+
+class GameModel extends BaseModel {
+  defaults() {
+    return {
+      playerOne: null,
+      playerTwo: null,
+      playerOneMove: null,
+      playerTwoMove: null,
+      winner: null
+    };
+  }
+
+
+  constructor() {
+    super('game');
+  }
+}
+
+export default GameModel;
